2af65d25d0366b72933c99d2690578312a8c1b66
kiana <kianaleslie25@gmail.com>
2023-09-07 12:42:29 -0600

    updates to first person scene and to turret exercise scene

--- TestInputProject/Assets/Scripts/MovementScript.cs
+++ TestInputProject/Assets/Scripts/MovementScript.cs
@@ -10,9 +10,11 @@ public class MovementScript : MonoBehaviour
     public InputAction rotateAction;
     Vector2 moveValue;
     Vector2 rotateValue;
+    Vector2 angles;
     //movement variables
     float moveSpeed;
     float rotateSpeed;
+    bool touchingGround = true;
     [SerializeField] GameObject weapon;
     void Start()
     {
@@ -27,10 +29,29 @@ public class MovementScript : MonoBehaviour
         moveValue = moveAction.ReadValue<Vector2>();
         rotateValue = rotateAction.ReadValue<Vector2>();
 
-        //rotate player and weapon 
+        //rotate player and turret 
         transform.Rotate(Vector3.up, rotateValue.x * rotateSpeed * Time.deltaTime);
-
         weapon.transform.Rotate(Vector3.right, rotateValue.y * rotateSpeed * Time.deltaTime);
+        //get current angles
+        angles = weapon.transform.eulerAngles;
+        //check if angles need to be clamped 
+        if (angles.x > 45.0f && angles.x < 180.0f)
+        {
+            weapon.transform.localRotation = Quaternion.Euler(45.0f, 0, 0); 
+        }
+        if (angles.x < 315.0f && angles.x > 180.0f)
+        {
+            weapon.transform.rotation = Quaternion.Euler(315.0f, 0, 0);
+        }
+        var kB = Keyboard.current;
+        if(kB != null)
+        {
+            if(kB.spaceKey.wasPressedThisFrame && touchingGround)
+            {
+                GetComponent<Rigidbody>().AddForce(Vector3.up * 300.0f * Time.deltaTime, ForceMode.Impulse/*VelocityChange*/);
+                touchingGround = false;
+            }
+        }
     }
     private void FixedUpdate()
     {
@@ -48,5 +69,20 @@ public class MovementScript : MonoBehaviour
     {
         moveAction.Disable();
         rotateAction.Disable();
+    }
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    touchingGround = true;
+    //}
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    touchingGround = false;
+    //}
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            touchingGround = true;
+        }
     }
 }
--- TestInputProject/Assets/Scripts/TankController.cs
+++ TestInputProject/Assets/Scripts/TankController.cs
@@ -6,13 +6,18 @@ public class TankController : MonoBehaviour
     //variables
     public InputAction leftMovementAction;
     public InputAction rightMovementAction;
+    public InputAction rotateA;
     Vector2 leftMoveValue;
     Vector2 rightMoveValue;
+    Vector2 rotateV;
     float movementSpeed;
+    float rotateS;
+    [SerializeField] GameObject turret;
     void Start()
     {
         //intialize
         movementSpeed = 2.0f;
+        rotateS = 100.0f;
     }
 
     void Update()
@@ -20,21 +25,28 @@ public class TankController : MonoBehaviour
         //keyboard input 
         leftMoveValue = leftMovementAction.ReadValue<Vector2>();
         rightMoveValue = rightMovementAction.ReadValue<Vector2>();
+        rotateV = rotateA.ReadValue<Vector2>();
+
+        turret.transform.Rotate(Vector3.right, rotateV.y * rotateS * Time.deltaTime);
     }
     private void FixedUpdate()
     {
         //move the object
         transform.Translate(new Vector3(leftMoveValue.x, 0, leftMoveValue.y) * movementSpeed * Time.deltaTime);
         transform.Translate(new Vector3(rightMoveValue.x, 0, rightMoveValue.y) * movementSpeed * Time.deltaTime);
+
+        turret.transform.Rotate(Vector3.right, rotateV.y * rotateS * Time.deltaTime);
     }
     private void OnEnable()
     {
         leftMovementAction.Enable();
         rightMovementAction.Enable();
+        rotateA.Enable();
     }
     private void OnDisable()
     {
         leftMovementAction.Disable();
         rightMovementAction.Disable();
+        rotateA.Disable();
     }
 }
