024acae2fe442580d4443aae0f961bb052a2ae63
kiana <kianaleslie25@gmail.com>
2023-09-24 15:49:42 -0600

    makin money moves

--- PlayerController_KianaLeslie/Assets/Scripts/FrontLoaderScript.cs
+++ PlayerController_KianaLeslie/Assets/Scripts/FrontLoaderScript.cs
@@ -1,14 +1,20 @@
+using System.Threading;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEngine.GraphicsBuffer;
 
 public class FrontLoaderScript : MonoBehaviour
 {
-    //input variables 
+    //input and movement variables 
     public InputAction moveAction;
     public InputAction rotateAction;
+    public InputAction armAction;
+    public InputAction bucketAction;
 
     Vector2 moveValue;
     Vector2 rotateValue;
+    Vector2 armValue;
+    Vector2 bucketValue;
     Vector2 angles;
 
     float moveSpeed;
@@ -18,8 +24,10 @@ public class FrontLoaderScript : MonoBehaviour
     [SerializeField] GameObject bucket;
 
     Keyboard kb;
+
     void Start()
     {
+        //initalize movement variables
         moveSpeed = 3.0f;
         rotateSpeed = 100.0f;
     }
@@ -28,15 +36,30 @@ public class FrontLoaderScript : MonoBehaviour
         //get player input 
         moveValue = moveAction.ReadValue<Vector2>();
         rotateValue = rotateAction.ReadValue<Vector2>();
+        armValue = armAction.ReadValue<Vector2>();
+        bucketValue = bucketAction.ReadValue<Vector2>();
+
+        //rotate turret left/right and barrel up/down
+        arm.transform.Rotate(Vector3.up, rotateValue.x * rotateSpeed * Time.deltaTime);
+        bucket.transform.Rotate(Vector3.left, bucketValue.y * rotateSpeed * Time.deltaTime);
     }
     private void FixedUpdate()
     {
+        //move and rotate objects
+        transform.Translate(new Vector3(moveValue.x, 0, moveValue.y) * moveSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.up, rotateValue.y * rotateSpeed * Time.deltaTime);
+
+
         arm.transform.Rotate(Vector3.up, rotateValue.x * rotateSpeed * Time.deltaTime);
         angles = arm.transform.localRotation.eulerAngles;
+
+        //clamp arms 60 degrees up
         if (angles.x > 25 && angles.x < 90)
         {
             arm.transform.localRotation = Quaternion.Euler(25.0f, 0, 0);
         }
+
+        //and 25 degrees down
         if (angles.x < 300 && angles.x > 270)
         {
             arm.transform.localRotation = Quaternion.Euler(300.0f, 0, 0);
@@ -45,10 +68,13 @@ public class FrontLoaderScript : MonoBehaviour
 
         bucket.transform.Rotate(Vector3.right, rotateValue.y * rotateSpeed * Time.deltaTime);
         angles = bucket.transform.localRotation.eulerAngles;
+
+        //clamp bucket rotation 40 degrees up 
         if (angles.x > 70 && angles.x < 90)
         {
             bucket.transform.localRotation = Quaternion.Euler(70.0f, 0, 0);
         }
+        //and 70 degrees down
         if (angles.x < 320 && angles.x > 270)
         {
             bucket.transform.localRotation = Quaternion.Euler(320.0f, 0, 0);
@@ -59,5 +85,15 @@ public class FrontLoaderScript : MonoBehaviour
         {
             Application.Quit();
         }
+    }
+    private void OnEnable()
+    {
+        moveAction.Enable();
+        rotateAction.Enable();
+    }
+    private void OnDisable()
+    {
+        moveAction.Disable();
+        rotateAction.Disable();
     }
 }
