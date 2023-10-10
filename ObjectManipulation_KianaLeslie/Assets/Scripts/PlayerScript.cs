95e49cb81b9f6d5f8aa176ef8814c902f3aa471a
kiana <kianaleslie25@gmail.com>
2023-10-09 18:12:43 -0600

    stay updating and pushing, this proj already crashed once

---
+++ ObjectManipulation_KianaLeslie/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerScript : MonoBehaviour
+{
+    public InputAction moveAction;
+    public InputAction rotateAction;
+
+    Vector2 moveValue;
+    Vector2 rotateValue;
+    Vector2 angles;
+
+    float moveSpeed;
+    float rotateSpeed;
+
+    bool touchingGround;
+    [SerializeField] GameObject weapon;
+    Keyboard kb;
+    void Start()
+    {
+        moveSpeed = 4.0f;
+        rotateSpeed = 100.0f;
+        kb = Keyboard.current;
+    }
+    void Update()
+    {
+        //get player input 
+        moveValue = moveAction.ReadValue<Vector2>();
+        rotateValue = rotateAction.ReadValue<Vector2>();
+
+        //rotate player and weapon
+        transform.Rotate(Vector3.up, rotateValue.x * rotateSpeed * Time.deltaTime);
+
+        weapon.transform.Rotate(Vector3.right, rotateValue.y * rotateSpeed * Time.deltaTime);
+        //get current angles
+        angles = weapon.transform.localEulerAngles;
+        //check if angles need to be clamped 
+        if (angles.x > 45.0f && angles.x < 180.0f)
+        {
+            weapon.transform.localRotation = Quaternion.Euler(45.0f, 0, 0);
+        }
+        if (angles.x < 315.0f && angles.x > 180.0f)
+        {
+            weapon.transform.rotation = Quaternion.Euler(315.0f, 0, 0);
+        }
+        if (kb.escapeKey.wasPressedThisFrame)
+        {
+            Application.Quit();
+        }
+    }
+    private void FixedUpdate()
+    {
+        //move the object
+        transform.Translate(new Vector3(moveValue.x, 0, moveValue.y) * moveSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.up, rotateValue.y * rotateSpeed * Time.deltaTime);
+    }
+    private void OnEnable()
+    {
+        moveAction.Enable();
+        rotateAction.Enable();
+    }
+    private void OnDisable()
+    {
+        moveAction.Disable();
+        rotateAction.Disable();
+    }
+}
