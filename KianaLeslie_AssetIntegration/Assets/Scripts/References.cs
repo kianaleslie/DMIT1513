afa53d23257b9f260d747aab8f86da06dddef507
kiana <kianaleslie25@gmail.com>
2023-11-13 16:24:15 -0700

    idk

---
+++ KianaLeslie_AssetIntegration/Assets/Scripts/References.cs
@@ -0,0 +1,12 @@
+
+//wooden toy asset
+//https://assetstore.unity.com/packages/3d/props/children-s-wooden-toys-free-train-pack-179951
+
+//playground asset 
+//https://assetstore.unity.com/packages/3d/environments/playground-low-poly-191533
+
+//rubber duck asset
+//https://assetstore.unity.com/packages/3d/props/super-rubber-duck-pack-34781
+
+//block asset
+//https://assetstore.unity.com/packages/3d/environments/block-world-68107
