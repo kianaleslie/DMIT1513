c1bc2bfceb8ec51aa014c36985339ed028d4b580
kiana <kianaleslie25@gmail.com>
2023-11-24 23:40:40 -0700

    .

--- KianaLeslie_AssetIntegration/Assets/Scripts/References.cs
+++ KianaLeslie_AssetIntegration/Assets/Scripts/References.cs
@@ -3,4 +3,8 @@
 //https://www.youtube.com/watch?v=MWQv2Bagwgk
 //https://assetstore.unity.com/packages/3d/environments/landscapes/terrain-sample-asset-pack-145808
 
-//
+//wolf asset
+//https://assetstore.unity.com/packages/3d/characters/animals/wolf-animated-45505
+
+//town assets
+//https://assetstore.unity.com/packages/3d/characters/animals/wolf-animated-45505
