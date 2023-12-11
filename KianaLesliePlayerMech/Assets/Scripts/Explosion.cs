8d6dcd4453953eb8c9813c8ccada6af18d3f2256
kiana <kianaleslie25@gmail.com>
2023-12-10 21:41:10 -0700

    uh oh

---
+++ KianaLesliePlayerMech/Assets/Scripts/Explosion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 explosionPos = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, 5.0f);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+            if (rb != null)
+                rb.AddExplosionForce(500.0f, explosionPos, 5.0f, 3.0F);
+        }
+    }
+}
---
+++ KianaLesliePlayerMech/Assets/Scripts/Health.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    // Health Variables
+    [SerializeField] int maxHealth, currentHealth;
+
+    [SerializeField] Image healthUI;
+    [SerializeField] TMP_Text healthValue;
+    [SerializeField] GameObject destructible;
+    [SerializeField] GameObject explosionParticle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHealth = 20;
+        currentHealth = maxHealth;
+        healthValue.text = currentHealth.ToString();
+    }
+
+    public void ApplyDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthUI.transform.localScale = new Vector3(currentHealth / (float)maxHealth, 1, 1);
+        healthValue.text = currentHealth.ToString();
+
+        if (currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        gameObject.SetActive(false);
+        if (destructible != null)
+        {
+            Instantiate(destructible, transform.position, transform.rotation);
+        }
+        if (explosionParticle != null)
+        {
+            Instantiate(explosionParticle, transform.position, transform.rotation);
+        }
+    }
+}
